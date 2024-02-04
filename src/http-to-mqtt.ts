974126aa5d789a97aae1f02acf701c03e5841eba
atorber <atorber@163.com>
2024-02-04 18:21:42 +0800

    0.6.0 实现http请求透传

--- src/http-to-mqtt.ts
+++ src/http-to-mqtt.ts
@@ -5,8 +5,7 @@ import { decrypt, encrypt, getKey, DecryptedMessage } from './utils.js'
 import jsonata from 'jsonata'
 import { v4 } from 'uuid'
 
-// 定义接口：请求头部信息
-type Headers = {
+type MqttOptions = {
   endpoint: string;
   port?: number;
   username: string;
@@ -15,11 +14,17 @@ type Headers = {
   secretkey?: string;
 }
 
+// 定义接口：请求头部信息
+type Headers = {
+  [key:string]: string;
+}
+
 // 定义接口：请求查询参数
 type Query = {
-  requesttopic: string;
+  requesttopic?: string;
   responsetopic?: string;
   convert?: string;
+  [key:string]: string | undefined;
 }
 
 // 定义接口：请求体内容
@@ -38,6 +43,8 @@ type Options = {
   headers: Headers;
   body: Body;
   query: Query;
+  method:'POST'|'GET'|'PUT'|'DELETE';
+  path: string;
 }
 
 // 主类：处理HTTP到MQTT的转换
@@ -45,8 +52,15 @@ class Http2Mqtt {
 
   private responsePayload: ResponsePayload = { body: {}, status: 200 }
   private ops: Options
+  private mqttOps: MqttOptions
+  private configOps: {
+    requesttopic?: string;
+    responsetopic?: string;
+    convert?: string;
+  }
 
   constructor (ops: Options) {
+    this.mqttOps = {} as MqttOptions
     const headers = Object.fromEntries(
       Object.entries(ops.headers).map(([ key, value ]) => [ key.toLowerCase(), value ]),
     )
@@ -54,18 +68,37 @@ class Http2Mqtt {
       console.log('headers中包含authorization\n', headers)
       // 对Authorization头部进行解析，删除开头的Bearer ，并对token进行base64解密
       const authorization = headers['authorization'] as string
-      const token = authorization.split(' ')[1] as string
-      const decodedToken = Buffer.from(token, 'base64').toString('utf-8')
+      const tokenJoin = authorization.split(' ')[1] as string
+      let mqttToken = ''
+      let token = ''
+      if (tokenJoin.includes('http2mqtt')) {
+        mqttToken = tokenJoin.split('http2mqtt')[0] || ''
+        token = tokenJoin.split('http2mqtt')[1] || ''
+        ops.headers['authorization'] = `Bearer ${token}`
+      } else {
+        mqttToken = tokenJoin
+      }
+      const decodedToken = Buffer.from(mqttToken, 'base64').toString('utf-8')
       console.log('decodedToken\n', decodedToken)
-      ops.headers = Object.fromEntries(
+      this.mqttOps = Object.fromEntries(
         Object.entries(JSON.parse(decodedToken)).map(([ key, value ]) => [ key.toLowerCase(), value ]),
-      ) as Headers
+      ) as MqttOptions
     } else {
       ops.headers = headers as Headers
+      this.mqttOps = headers as unknown as MqttOptions
     }
-    ops.query = Object.fromEntries(
-      Object.entries(ops.query).map(([ key, value ]) => [ key.toLowerCase(), value ]),
-    ) as Query
+    const { requesttopic, responsetopic, convert } = ops.query
+    this.configOps = {
+      requesttopic, responsetopic, convert,
+    }
+    // ops.query = Object.fromEntries(
+    //   Object.entries(ops.query).map(([ key, value ]) => [ key.toLowerCase(), value ]),
+    // ) as Query
+
+    if (ops.query.convert) delete ops.query.convert
+    if (ops.query.requesttopic) delete ops.query.requesttopic
+    if (ops.query.responsetopic) delete ops.query.responsetopic
+
     this.ops = ops
   }
 
@@ -77,9 +110,9 @@ class Http2Mqtt {
       password = '',
       port = 1883,
       secretkey: key,
-    } = this.ops.headers
+    } = this.mqttOps
 
-    const { requesttopic, responsetopic, convert } = this.ops.query
+    const { requesttopic, responsetopic, convert } = this.configOps
     let pubTopic = requesttopic || ''
     let subTopic = responsetopic || ''
 
@@ -94,7 +127,7 @@ class Http2Mqtt {
       subTopic = `http2mqtt/response/${reqId}`
     }
 
-    let payload: any = this.ops.body
+    let payload: any = this.ops
 
     // 如果存在Convert参数，使用jsonata进行数据转换
     if (convert) {
